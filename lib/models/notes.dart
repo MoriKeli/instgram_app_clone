586d497bd9979a5872788f1de255c2d60f295d7f
magnus.coder <kelimbth@gmail.com>
2024-01-09 08:02:19 +0300

    created a model for voice notes

---
+++ lib/models/notes.dart
@@ -0,0 +1,40 @@
+class VoiceNotes{
+  final String username;
+  final String profilePic;
+
+  VoiceNotes({
+    required this.username,
+    required this.profilePic,
+  });
+}
+
+List<VoiceNotes> voiceNotes = [
+  VoiceNotes(
+    profilePic: "assets/images/dps/testuser.jpg",
+    username: 'Add story',
+  ),
+  VoiceNotes(
+    profilePic: "assets/images/dps/sarah-johnson.jpg",
+    username: 'sarah.johnson',
+  ),
+  VoiceNotes(
+    profilePic: "assets/images/dps/brenda-jones.jpg",
+    username: 'iambrendajones',
+  ),
+  VoiceNotes(
+    profilePic: "assets/images/dps/coder-girl.jpg",
+    username: 'coder.girl',
+  ),
+  VoiceNotes(
+    profilePic: "assets/images/dps/kelvin-anderson.jpg",
+    username: 'kelvin.anderson',
+  ),
+  VoiceNotes(
+    profilePic: "assets/images/dps/vanguard.jpg",
+    username: 'van_guard',
+  ),
+  VoiceNotes(
+    profilePic: "assets/images/dps/yule-msee.jpg",
+    username: 'yule.msee',
+  ),
+];
