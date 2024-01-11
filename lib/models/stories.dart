62d2531b62d8ae6f245cc7aba2793a57d8cec2dc
magnus.coder <kelimbth@gmail.com>
2024-01-11 19:41:53 +0300

    minor changes: 1.) updated assets dir. path 2.) added 'isViewed' property

--- lib/models/stories.dart
+++ lib/models/stories.dart
@@ -1,40 +1,46 @@
 class InstagramStories {
   final String profilePic;
   final String username;
+  final bool isViewed;
 
   InstagramStories({
     required this.profilePic,
     required this.username,
+    this.isViewed = false,
   });
 }
 
 List<InstagramStories> stories = [
   InstagramStories(
-    profilePic: "images/dps/testuser.jpg",
+    profilePic: "assets/images/dps/testuser.jpg",
     username: 'Add story',
+    isViewed: true,
   ),
   InstagramStories(
-    profilePic: "images/dps/sarah-johnson.jpg",
+    profilePic: "assets/images/dps/sarah-johnson.jpg",
     username: 'sarah.johnson',
+    isViewed: true,
   ),
   InstagramStories(
-    profilePic: "images/dps/brenda-jones.jpg",
+    profilePic: "assets/images/dps/brenda-jones.jpg",
     username: 'iambrendajones',
+    isViewed: true,
   ),
   InstagramStories(
-    profilePic: "images/dps/coder-girl.jpg",
+    profilePic: "assets/images/dps/coder-girl.jpg",
     username: 'coder.girl',
   ),
   InstagramStories(
-    profilePic: "images/dps/kelvin-anderson.jpg",
+    profilePic: "assets/images/dps/kelvin-anderson.jpg",
     username: 'kelvin.anderson',
   ),
   InstagramStories(
-    profilePic: "images/dps/vanguard.jpg",
+    profilePic: "assets/images/dps/vanguard.jpg",
     username: 'van_guard',
+    isViewed: true,
   ),
   InstagramStories(
-    profilePic: "images/dps/yule-msee.jpg",
+    profilePic: "assets/images/dps/yule-msee.jpg",
     username: 'yule.msee',
   ),
 ];
