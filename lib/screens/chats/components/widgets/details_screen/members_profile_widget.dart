b3f57b292978426282c7e187d51870a067c514c2
magnus.coder <kelimbth@gmail.com>
2024-02-05 15:21:58 +0300

    created a widget to display recipient's account info

---
+++ lib/screens/chats/components/widgets/details_screen/members_profile_widget.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:instagram_clone/constants/colors.dart';
+
+
+class MembersProfileWidget extends StatelessWidget {
+  const MembersProfileWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: CircleAvatar(
+        radius: 24.0,
+        backgroundImage: AssetImage('images/dps/brenda-jones.jpg'),
+      ),
+      title: Text('iambrendajones', style: TextStyle(fontSize: 14.0)),
+      subtitle: Text('iambrendajones', style: TextStyle(color: kTextSecondaryColor, fontSize: 13.0)),
+      trailing: SizedBox(
+        height: 32.0,
+        child: ElevatedButton(
+          onPressed: () {},
+          style: ButtonStyle(
+            backgroundColor: WidgetStatePropertyAll(kSecondaryColor),
+            shape: WidgetStatePropertyAll(RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(4.0))),
+          ),
+          child: Text(
+            'Following',
+            style: TextStyle(color: kTextColor),
+          ),
+        ),
+      ),
+    );
+  }
+}
