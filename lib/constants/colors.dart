b57c32715794c7d2d3b1f217fca35cace5f76466
code.magnate <kelimbth@gmail.com>
2024-08-17 15:25:56 +0300

    added constants -> colors

---
+++ lib/constants/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const kBackgroundColor = Color.fromRGBO(0, 0, 0, 1);
+const kSearchBarColor = Color.fromRGBO(38, 38, 38, 1);
+const kBlueColor = Color.fromRGBO(0, 149, 246, 1);
+const kPrimaryColor = Colors.white;
+const kSecondaryColor = Colors.grey;
