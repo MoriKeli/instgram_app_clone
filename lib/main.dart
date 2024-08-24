2fb82f9a917df7430c1c9d617a85426e05de9049
code.magnate <kelimbth@gmail.com>
2024-08-24 15:38:48 +0300

    imported 'routes' file

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:instagram_clone/constants/colors.dart';
+import 'package:instagram_clone/routes.dart';
 import 'package:instagram_clone/screens/homepage.dart';
 import 'package:instagram_clone/screens/login.dart';
 
