15eef533bf0ae0d05abc2e4a105bf31e4ca1e1b3
code.magnate <kelimbth@gmail.com>
2024-08-27 15:39:27 +0300

    updated widget name for LoginScreen()

--- lib/routes.dart
+++ lib/routes.dart
@@ -3,6 +3,6 @@ import 'package:instagram_clone/screens/homepage.dart';
 import 'package:instagram_clone/screens/login.dart';
 
 final Map<String, WidgetBuilder> routes = {
-  LoginPage.routeName: (context) => LoginPage(),
+  LoginScreen.routeName: (context) => LoginScreen(),
   Homepage.routeName: (context) => Homepage(),
 };
