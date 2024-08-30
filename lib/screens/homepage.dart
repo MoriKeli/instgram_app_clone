c35e1eaaed85f237182824fd5a386fd466d6a68a
code.magnate <kelimbth@gmail.com>
2024-08-30 15:41:04 +0300

    bug-fix: updated routeName to redirect to homepage - not login screen

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class Homepage extends StatefulWidget {
-  static String routeName = '/login';
+  static String routeName = '/homepage';
   const Homepage({super.key});
 
   @override
