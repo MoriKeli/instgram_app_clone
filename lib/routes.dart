ef770aa38265ebf58532652005c9306dda306b1b
code.magnate <kelimbth@gmail.com>
2024-08-31 17:02:02 +0300

    replaced Homepage.routeName with HomeScreen.routeName

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:instagram_clone/screens/homepage.dart';
+import 'package:instagram_clone/common/homescreen.dart';
 import 'package:instagram_clone/screens/login.dart';
 
 final Map<String, WidgetBuilder> routes = {
   LoginScreen.routeName: (context) => LoginScreen(),
-  Homepage.routeName: (context) => Homepage(),
+  HomeScreen.routeName: (context) => HomeScreen(),
 };
