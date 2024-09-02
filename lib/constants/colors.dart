004278f848df077b25bd619a1458c085be8c39f6
code.magnate <kelimbth@gmail.com>
2024-09-02 17:02:57 +0300

    added new colors for appbar and icons

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 const kBackgroundColor = Color.fromRGBO(0, 0, 0, 1);
+const kAppBarBackgroundColor = Color.fromRGBO(0, 0, 0, 1);
 const kSearchBarColor = Color.fromRGBO(38, 38, 38, 1);
 const kBlueColor = Color.fromRGBO(0, 149, 246, 1);
 const kPrimaryColor = Colors.white;
 const kSecondaryColor = Colors.grey;
+const kIconColor = Colors.white;
