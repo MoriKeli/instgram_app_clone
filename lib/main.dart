b529e7fb5c0e06ef65f0817dcc70de1040f23e7e
code.magnate <kelimbth@gmail.com>
2024-09-02 17:04:41 +0300

    imported new module

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:instagram_clone/constants/colors.dart';
 import 'package:instagram_clone/routes.dart';
+import 'package:instagram_clone/screens/login.dart';
 
 void main() {
   runApp(const InstagramAppClone());
