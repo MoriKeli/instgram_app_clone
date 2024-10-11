ebe044e16f68efe577b25cefee298cf0464ffe9e
code.magnate <kelimbth@gmail.com>
2024-10-11 15:20:41 +0300

    created a login screen button component

---
+++ lib/screens/login/components/widgets/login_screen_btn.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+
+class LoginButton extends StatelessWidget {
+  const LoginButton({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 5.0),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.zero,
+        shape: BoxShape.rectangle
+      ),
+      width: double.infinity,
+      child: TextButton(
+        style: ButtonStyle(
+          backgroundColor: WidgetStateProperty.all(Colors.blueAccent),
+          padding: WidgetStateProperty.all(const EdgeInsets.all(10.0)),
+          shape: WidgetStateProperty.all(BeveledRectangleBorder(borderRadius: BorderRadius.circular(2.0))),
+        ),
+        onPressed: () {
+          Navigator.pushNamed(context, '/homescreen');
+        },
+        child: const Text("login", style: TextStyle(fontSize: 16.0)),
+      ),
+    );
+  }
+}
+
