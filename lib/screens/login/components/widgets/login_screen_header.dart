96de683e11c366799946e8d7a81115663f0c8348
code.magnate <kelimbth@gmail.com>
2024-10-14 09:11:29 +0300

    created a component for login screen header

---
+++ lib/screens/login/components/widgets/login_screen_header.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class LoginScreenHeader extends StatelessWidget {
+  const LoginScreenHeader({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.center,
+      padding: const EdgeInsets.only(top: 80.0, bottom: 50.0),
+      child: const Image(
+        image: AssetImage("images/logo-black-bg.jpg"),
+        height: 70.0,
+      ),
+    );
+  }
+}
