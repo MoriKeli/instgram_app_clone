f1898e5221eac0eb26b58d97e6580564fa5d987b
code.magnate <kelimbth@gmail.com>
2024-10-15 17:29:12 +0300

    created a component for the bottom navbar in login screen

---
+++ lib/screens/login/components/widgets/signin_with_facebook.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+
+class SignInWithFacebook extends StatelessWidget {
+  const SignInWithFacebook({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Padding(
+      padding: EdgeInsets.all(20.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image(
+            image: AssetImage("images/fb-icon.png"),
+            height: 20.0,
+          ),
+          Text(" Continue as "),
+          Text(
+            "test user",
+            style: TextStyle(
+              fontWeight: FontWeight.w500,
+              color: Colors.blue,
+            )
+          ),
+        ],
+      ),
+    );
+  }
+}
+
