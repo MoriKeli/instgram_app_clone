fd63810df154ed0f8fa603b7da281b0482cae17a
code.magnate <kelimbth@gmail.com>
2024-10-18 13:52:23 +0300

    updated path for login screen module

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:instagram_clone/common/homescreen.dart';
-import 'package:instagram_clone/screens/login.dart';
+import 'package:instagram_clone/screens/login/login.dart';
 
 final Map<String, WidgetBuilder> routes = {
   LoginScreen.routeName: (context) => LoginScreen(),
