756f5e93644c0e9412d3103b9ce0e1eb035c34d1
magnus.coder <kelimbth@gmail.com>
2024-10-31 09:48:26 +0300

    refactored-code: deleted lists containing data

--- lib/screens/homepage/homepage.dart
+++ lib/screens/homepage/homepage.dart
@@ -11,26 +11,6 @@ class HomeScreenWidgets extends StatefulWidget {
 }
 
 class _HomeScreenWidgetsState extends State<HomeScreenWidgets> {
-  List<String> profileImages = [
-    "images/dps/boy-1.jpg",
-    "images/dps/boy-2.jpg",
-    "images/dps/boy-3.jpg",
-    "images/dps/boy-4.jpg",
-    "images/dps/girl-1.jpg",
-    "images/dps/girl-2.jpg",
-    "images/dps/girl-3.jpg",
-  ];
-
-  List<String> userPosts = [
-    "images/1.jpg",
-    "images/2.jpg",
-    "images/3.jpg",
-    "images/5.jpg",
-    "images/6.jpg",
-    "images/13.jpg",
-    "images/gallery-8.jpg",
-  ];
-
   //function to refresh page
   Future<void> refreshPage() async {
     await Future.delayed(const Duration(seconds: 1));
