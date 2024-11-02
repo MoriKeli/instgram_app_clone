d1763aba6a7da4c0d1501bd15b458e90596e6de0
magnus.coder <kelimbth@gmail.com>
2024-11-02 08:20:27 +0300

    created a model for instagram stories

---
+++ lib/models/stories.dart
@@ -0,0 +1,40 @@
+class InstagramStories {
+  final String profilePic;
+  final String username;
+
+  InstagramStories({
+    required this.profilePic,
+    required this.username,
+  });
+}
+
+List<InstagramStories> stories = [
+  InstagramStories(
+    profilePic: "images/dps/testuser.jpg",
+    username: 'Add story',
+  ),
+  InstagramStories(
+    profilePic: "images/dps/sarah-johnson.jpg",
+    username: 'sarah.johnson',
+  ),
+  InstagramStories(
+    profilePic: "images/dps/brenda-jones.jpg",
+    username: 'iambrendajones',
+  ),
+  InstagramStories(
+    profilePic: "images/dps/coder-girl.jpg",
+    username: 'coder.girl',
+  ),
+  InstagramStories(
+    profilePic: "images/dps/kelvin-anderson.jpg",
+    username: 'kelvin.anderson',
+  ),
+  InstagramStories(
+    profilePic: "images/dps/vanguard.jpg",
+    username: 'van_guard',
+  ),
+  InstagramStories(
+    profilePic: "images/dps/yule-msee.jpg",
+    username: 'yule.msee',
+  ),
+];
