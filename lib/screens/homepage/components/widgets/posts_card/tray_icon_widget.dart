9a63242154de8d53317337934f063d0f4fde4798
magnus.coder <kelimbth@gmail.com>
2024-11-08 16:20:54 +0300

    created a tray icon widget like, comment,  message and save icons

---
+++ lib/screens/homepage/components/widgets/posts_card/tray_icon_widget.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+
+class TrayIcon extends StatelessWidget {
+  const TrayIcon({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(left: 10.0, right: 4.0),
+          child: InkWell(
+            onTap: () {},
+            child: Icon(Icons.favorite, color: Colors.red, size: 22.0),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 4.0),
+          child: InkWell(
+            onTap: () {},
+            child: ImageIcon(
+              AssetImage('images/icons/comment-icon.png'),
+              size: 28.0,
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 4.0),
+          child: InkWell(
+            onTap: () {},
+            child: ImageIcon(
+              AssetImage('images/icons/message-icon.png'),
+              size: 22.0,
+            ),
+          ),
+        ),
+        Spacer(),
+        IconButton(
+          onPressed: () {},
+          icon: Icon(
+            Icons.bookmark_outline,
+            size: 22.0,
+          ),
+        ),
+      ],
+    );
+  }
+}
