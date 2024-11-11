c6dffbad5a38494b9228449f76a7a649b0bba0b5
magnus.coder <kelimbth@gmail.com>
2024-11-11 13:00:28 +0300

    created a widget to view comments of a post

---
+++ lib/screens/homepage/components/widgets/posts_card/comments_widget.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:instagram_clone/constants/colors.dart';
+import 'package:instagram_clone/models/posts.dart';
+
+
+class CommentsWidget extends StatelessWidget {
+  const CommentsWidget({
+    super.key,
+    required this.userFeed,
+    required this.currentPostIndex,
+  });
+
+  final List<Posts> userFeed;
+  final int currentPostIndex;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 12.0),
+      child: Row(
+        children: [
+          Text(
+            'View all ${userFeed[currentPostIndex].totalComments} comments',
+            style: TextStyle(
+              color: kSecondaryColor,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
