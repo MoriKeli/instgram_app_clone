a2ea17568a4cdc8405ca5fa4ab903baef4fc5bc2
magnus.coder <kelimbth@gmail.com>
2024-11-12 08:26:54 +0300

    updated appbar 1.) deleted leading section 2.) set 'automaticallyImplyLeading' to prevent the 'arrow' icon from being shown in the homescreen 3.) updated notifications and message icons 4.) used an ImageIcon() for messages 5.) extracted widget for notifications and messages icons

--- lib/screens/homepage/components/appbar.dart
+++ lib/screens/homepage/components/appbar.dart
@@ -4,26 +4,85 @@ import 'package:instagram_clone/constants/colors.dart';
 
 AppBar appBar() {
   return AppBar(
-    leading: Padding(
-      padding: const EdgeInsets.only(left: 10.0),
-      child: const Icon(Icons.camera_alt_outlined, size: 30.0),
-    ),
-    leadingWidth: 27.0,
+    automaticallyImplyLeading: false,
     title: const Image(
       image: AssetImage("images/logo-black-bg.jpg"),
       height: 50.0,
-      // fit: BoxFit.contain,
     ),
     backgroundColor: kBackgroundColor,
     actions: [
-      IconButton(
-        icon: const Icon(Icons.favorite_border, size: 30.0),
-        onPressed: () {},
-      ),
-      IconButton(
-        icon: const Icon(Icons.send, size: 30.0),
-        onPressed: () {},
-      ),
+      NotificationsIcon(),
+      MessagesIcon(),
     ],
   );
+}
+
+class MessagesIcon extends StatelessWidget {
+  const MessagesIcon({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(right: 10.0),
+      child: Stack(
+        clipBehavior: Clip.none,
+        children: [
+          ImageIcon(
+            AssetImage('images/icons/message-icon.png'),
+            size: 28.0,
+          ),
+          Positioned(
+            bottom: 12,
+            left: 16,
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.red,
+                shape: BoxShape.circle,
+              ),
+              padding: EdgeInsets.all(5.0),
+              child: Text(
+                '2',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 12.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class NotificationsIcon extends StatelessWidget {
+  const NotificationsIcon({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        IconButton(
+          icon: const Icon(Icons.favorite_border, size: 30.0),
+          onPressed: () {},
+        ),
+        Positioned(
+          top: 12,
+          right: 9,
+          child: Container(
+            decoration: BoxDecoration(
+              color: Colors.red,
+              shape: BoxShape.circle,
+            ),
+            child: Icon(Icons.circle, color: Colors.red, size: 9.0),
+          ),
+        )
+      ],
+    );
+  }
 }
