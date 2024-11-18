45efebdcfac5ea88f0f1e33aec9da802d1f7fb72
magnus.coder <kelimbth@gmail.com>
2024-11-18 10:30:45 +0300

    created the body section for the signup screen

---
+++ lib/screens/signup/components/signup_screen_body.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:instagram_clone/screens/signup/components/signup_form.dart';
+
+
+class SignupScreenBody extends StatelessWidget {
+  const SignupScreenBody({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 12.0),
+        child: ListView(
+          children: [
+            Image.asset(
+              'images/logo-black-bg.jpg',
+              height: MediaQuery.of(context).size.height * .12,
+            ),
+            SizedBox(height: 5.0),
+            SignupForm(),   // signup form
+          ],
+        ),
+      ),
+    );
+  }
+}
