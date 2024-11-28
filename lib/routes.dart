eb5c1fdc65f3be8ab93e1904752db663d867c7d6
magnus.coder <kelimbth@gmail.com>
2024-11-28 09:46:31 +0300

    added route for the signup screen

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:instagram_clone/common/homescreen.dart';
 import 'package:instagram_clone/screens/login/login.dart';
+import 'package:instagram_clone/screens/signup/signup.dart';
 
 final Map<String, WidgetBuilder> routes = {
   LoginScreen.routeName: (context) => LoginScreen(),
+  SignupScreen.routeName: (context) => SignupScreen(),
   HomeScreen.routeName: (context) => HomeScreen(),
 };
