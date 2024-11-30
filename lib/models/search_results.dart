653e6bdb40e18ca1dfa3ab32577abf00718e11eb
magnus.coder <kelimbth@gmail.com>
2024-11-30 08:01:25 +0300

    created a model for suggested search results

---
+++ lib/models/search_results.dart
@@ -0,0 +1,44 @@
+class SearchResults {
+  final String imagePath;
+
+  SearchResults({
+    required this.imagePath,
+  });
+}
+
+List<SearchResults> searchResults = [
+  SearchResults(imagePath: "images/posts/hangout.jpg"),
+  SearchResults(imagePath: "images/posts/crusade.jpg"),
+  SearchResults(imagePath: "images/posts/event.jpg"),
+  SearchResults(imagePath: "images/posts/kelvin-anderson.jpg"),
+  SearchResults(imagePath: "images/posts/just-chilling.jpg", ),
+  SearchResults(imagePath: "images/posts/family-hangout.jpg"),
+  SearchResults(imagePath: "images/posts/male-developer.jpg"),
+  SearchResults(imagePath: "images/posts/img1.jpeg"),
+  SearchResults(imagePath: "images/posts/img2.jpeg"),
+  SearchResults(imagePath: "images/posts/img3.jpeg"),
+  SearchResults(imagePath: "images/posts/img4.jpeg"),
+  SearchResults(imagePath: "images/posts/img5.jpeg"),
+  SearchResults(imagePath: "images/posts/img6.jpeg"),
+  SearchResults(imagePath: "images/posts/img7.jpeg"),
+  SearchResults(imagePath: "images/posts/img8.jpeg"),
+  SearchResults(imagePath: "images/posts/img9.jpeg"),
+  SearchResults(imagePath: "images/posts/img10.jpeg"),
+  SearchResults(imagePath: "images/posts/img11.jpeg"),
+  SearchResults(imagePath: "images/posts/img12.jpeg"),
+  SearchResults(imagePath: "images/posts/img13.jpeg"),
+  SearchResults(imagePath: "images/posts/img14.jpeg"),
+  SearchResults(imagePath: "images/posts/img15.jpeg"),
+  SearchResults(imagePath: "images/posts/img16.jpeg"),
+  SearchResults(imagePath: "images/posts/img17.jpeg"),
+  SearchResults(imagePath: "images/posts/img18.jpeg"),
+  SearchResults(imagePath: "images/posts/img19.jpeg"),
+  SearchResults(imagePath: "images/posts/img20.jpeg"),
+  SearchResults(imagePath: "images/posts/img21.jpeg"),
+  SearchResults(imagePath: "images/posts/img22.jpeg"),
+  SearchResults(imagePath: "images/posts/img23.jpeg"),
+  SearchResults(imagePath: "images/posts/img24.jpeg"),
+  SearchResults(imagePath: "images/posts/img25.jpg"),
+  SearchResults(imagePath: "images/posts/img26.jpg"),
+
+];
