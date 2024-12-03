bb675c7ef76f5fb2ffa32135876e572b28ed7a3b
magnus.coder <kelimbth@gmail.com>
2024-12-03 14:25:40 +0300

    created a body component for search screen

---
+++ lib/screens/search/components/search_screen_body.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:instagram_clone/screens/search/components/search_input_field.dart';
+import 'package:instagram_clone/screens/search/components/suggested_search_results.dart';
+
+class SearchScreenBody extends StatelessWidget {
+  const SearchScreenBody({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: [
+        SearchInputField(),
+        SizedBox(height: 10.0),
+        SuggestedSearchResults(),   // images displayed before you search for an item
+      ],
+    );
+  }
+}
+
