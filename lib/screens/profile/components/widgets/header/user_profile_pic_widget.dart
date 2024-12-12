532e43dd738e1fb61055a672b964395dc314097d
magnus.coder <kelimbth@gmail.com>
2024-12-12 09:11:43 +0300

    created a widget to display profile picture in the profile screen header

---
+++ lib/screens/profile/components/widgets/header/user_profile_pic_widget.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:instagram_clone/constants/colors.dart';
+
+class UserProfilePictureWidget extends StatelessWidget {
+  const UserProfilePictureWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(clipBehavior: Clip.none, children: [
+      CircleAvatar(
+        radius: MediaQuery.of(context).size.width * .12,
+        backgroundImage: AssetImage('images/dps/testuser.jpg'),
+      ),
+      Positioned(
+        top: MediaQuery.of(context).size.height * .1,
+        left: MediaQuery.of(context).size.width * .14,
+        child: Container(
+          height: 20.0,
+          decoration: BoxDecoration(
+            color: kBlueColor,
+            shape: BoxShape.circle,
+          ),
+          padding: EdgeInsets.all(2.0),
+          child: IconButton(
+            padding: EdgeInsets.zero,
+            onPressed: () {},
+            icon: Icon(Icons.add, size: 16.0),
+          ),
+        ),
+      )
+    ]);
+  }
+}
