37bb8b0bcc692819b3b003d6983681f720d4832e
magnus.coder <kelimbth@gmail.com>
2024-12-18 15:00:49 +0300

    created a widget to display username above the user's bio

---
+++ lib/screens/profile/components/widgets/bio/username_widget.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class UsernameWidget extends StatelessWidget {
+  const UsernameWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      'testuser.original',
+      style: TextStyle(fontWeight: FontWeight.bold),
+    );
+  }
+}
