253bdfd49e82850fbeeb5afd00d44da396c83c00
magnus.coder <kelimbth@gmail.com>
2024-12-22 08:00:33 +0300

    added new routes for search and profile screens

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:instagram_clone/common/homescreen.dart';
 import 'package:instagram_clone/screens/login/login.dart';
+import 'package:instagram_clone/screens/profile/profile_screen.dart';
+import 'package:instagram_clone/screens/search/search_screen.dart';
 import 'package:instagram_clone/screens/signup/signup.dart';
 
 final Map<String, WidgetBuilder> routes = {
   LoginScreen.routeName: (context) => LoginScreen(),
   SignupScreen.routeName: (context) => SignupScreen(),
   HomeScreen.routeName: (context) => HomeScreen(),
+  SearchScreen.routeName: (context) => SearchScreen(),
+  ProfileScreen.routeName: (context) => ProfileScreen(),
 };
