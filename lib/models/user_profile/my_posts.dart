8db10455d2224fd5beece0866368ab631e2c9b69
magnus.coder <kelimbth@gmail.com>
2024-12-31 10:20:45 +0300

    created a model for a user posts that's shown in the profile screen

---
+++ lib/models/user_profile/my_posts.dart
@@ -0,0 +1,38 @@
+class MyPosts {
+  final String imagePath;
+
+  MyPosts({
+    required this.imagePath,
+  });
+}
+
+List<MyPosts> myPosts = [
+  MyPosts(imagePath: "images/posts/just-chilling.jpg", ),
+  MyPosts(imagePath: "images/posts/img1.jpeg"),
+  MyPosts(imagePath: "images/posts/img2.jpeg"),
+  MyPosts(imagePath: "images/posts/img3.jpeg"),
+  MyPosts(imagePath: "images/posts/img4.jpeg"),
+  MyPosts(imagePath: "images/posts/img5.jpeg"),
+  MyPosts(imagePath: "images/posts/img6.jpeg"),
+  MyPosts(imagePath: "images/posts/img7.jpeg"),
+  MyPosts(imagePath: "images/posts/img8.jpeg"),
+  MyPosts(imagePath: "images/posts/img9.jpeg"),
+  MyPosts(imagePath: "images/posts/img10.jpeg"),
+  MyPosts(imagePath: "images/posts/img11.jpeg"),
+  MyPosts(imagePath: "images/posts/img12.jpeg"),
+  MyPosts(imagePath: "images/posts/img13.jpeg"),
+  MyPosts(imagePath: "images/posts/img14.jpeg"),
+  MyPosts(imagePath: "images/posts/img15.jpeg"),
+  MyPosts(imagePath: "images/posts/img16.jpeg"),
+  MyPosts(imagePath: "images/posts/img17.jpeg"),
+  MyPosts(imagePath: "images/posts/img18.jpeg"),
+  MyPosts(imagePath: "images/posts/img19.jpeg"),
+  MyPosts(imagePath: "images/posts/img20.jpeg"),
+  MyPosts(imagePath: "images/posts/img21.jpeg"),
+  MyPosts(imagePath: "images/posts/img22.jpeg"),
+  MyPosts(imagePath: "images/posts/img23.jpeg"),
+  MyPosts(imagePath: "images/posts/img24.jpeg"),
+  MyPosts(imagePath: "images/posts/img25.jpg"),
+  MyPosts(imagePath: "images/posts/img26.jpg"),
+
+];
