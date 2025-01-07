f34790045a2fde5792402d48b1de7a34fd20c66f
magnus.coder <kelimbth@gmail.com>
2025-01-07 10:20:32 +0300

    created a widget to display posts the user was tagged

---
+++ lib/screens/profile/components/widgets/tabs/tagged_posts.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:instagram_clone/models/user_profile/my_posts.dart';
+
+
+class TaggedPostsWidget extends StatelessWidget {
+  const TaggedPostsWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      child: Wrap(
+        children: List.generate(
+          myPosts.length,
+          (index) => Column(
+            children: [
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: 2.0, vertical: 2.0),
+                child: Image.asset(
+                  myPosts[index].imagePath,
+                  fit: BoxFit.cover,
+                  height: MediaQuery.of(context).size.height * .18,   // height of each image (screen height x 0.18)
+                  width: MediaQuery.of(context).size.width * .32,   // width of each image (screen width x 0.32)
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
