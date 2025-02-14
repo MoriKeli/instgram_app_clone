ead95b04d752f18df7d52159450728a493512264
magnus.coder <kelimbth@gmail.com>
2025-02-14 08:22:14 +0300

    created a model for reels

---
+++ lib/models/reels.dart
@@ -0,0 +1,23 @@
+class Reels {
+  final String videoFilePath;
+
+  Reels({
+    required this.videoFilePath,
+  });
+}
+
+List<Reels> reels = [
+  Reels(videoFilePath: 'assets/videos/github-unwrapped.mp4'),
+  Reels(videoFilePath: 'assets/videos/reels1.mp4'),
+  Reels(videoFilePath: 'assets/videos/prank-gone-wrong.mp4'),
+  Reels(videoFilePath: 'assets/videos/reels2.mp4'),
+  Reels(videoFilePath: 'assets/videos/reels3.mp4'),
+  Reels(videoFilePath: 'assets/videos/fast-internet.mp4'),
+  Reels(videoFilePath: 'assets/videos/reels4.mp4'),
+  Reels(videoFilePath: 'assets/videos/reels5.mp4'),
+  Reels(videoFilePath: 'assets/videos/the-boys.mp4'),
+  Reels(videoFilePath: 'assets/videos/reels6.mp4'),
+  Reels(videoFilePath: 'assets/videos/reels7.mp4'),
+  Reels(videoFilePath: 'assets/videos/homeless.mp4'),
+
+];
